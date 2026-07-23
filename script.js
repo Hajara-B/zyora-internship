a795e38a3394f64a8755890118e82e62f06cd0bc
Hajara-B <hajarabasheer26@gamil.com>
2026-07-24 01:37:16 +0530

    Add dark mode toggle

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 console.log("Javascript connected successfully!");
+const darkMode=document.querySelector("#darkMode");
+darkMode.addEventListener("click",function(){
+    console.log("Button clicked!");
+    document.body.classList.toggle("darkMode");
+});
