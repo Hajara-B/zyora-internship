7daf5443c618b9be5fada56b361025eaaeeb0a9f
Hajara-B <hajarabasheer26@gamil.com>
2026-07-24 15:51:49 +0530

    Form Validation and Email Checking

--- script.js
+++ script.js
@@ -4,3 +4,25 @@ darkMode.addEventListener("click",function(){
     console.log("Button clicked!");
     document.body.classList.toggle("darkMode");
 });
+const contactForm = document.querySelector("#contactForm");
+
+contactForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const name = document.querySelector("#name").value;
+    const email = document.querySelector("#email").value;
+    const message = document.querySelector("#message").value;
+
+    console.log(name);
+    console.log(email);
+    console.log(message);
+
+    if (name === "" || email === "" || message === "") {
+        alert("Please fill in all fields.");
+        return;
+    }
+    if (!email.includes("@") || !email.includes(".")) {
+    alert("Enter a valid email.");
+    return;
+}
+});
